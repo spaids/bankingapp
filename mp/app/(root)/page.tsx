4586bc10bbc2678596eeba206e7c1d177603825e
spaids <junglegold3@gmail.com>
2024-11-11 23:41:51 -0600

    Test Edit

--- mp/app/(root)/page.tsx
+++ mp/app/(root)/page.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          Welcome, Nate
+        </header>
+      </div>
+    </section>
   )
 }
 
